d718b5a192cc1e300aa0ea6ec7a67ec209742af9
Marcel Gruenauer <bigghosthead@gmail.com>
2022-12-03 11:39:03 +0100

    bin/use-translations.pl: added some error checking

--- bin/use-translations.pl
+++ bin/use-translations.pl
@@ -3,9 +3,15 @@ use strict;
 use warnings;
 use v5.10;
 use Path::Tiny;
-my $js = join "\n",
-  map { sprintf qq!    "%s" : "%s",!, split /\t/ }
-  path('translations.tsv')->lines_utf8({ chomp => 1 });
+my @js;
+for (path('translations.tsv')->lines_utf8({ chomp => 1 })) {
+    if (/^(.*?)\t([^\t]*)$/) {
+        push @js, sprintf qq!    "%s" : "%s",!, $1, $2;
+    } else {
+        warn "[$_] does not contain exactly one tab\n";
+    }
+}
+my $js = join "\n", @js;
 path('content.js')->edit_utf8(
     sub {
         s/const textReplacements = \{\n\K.*?(?=\n}\n)/$js/s;
